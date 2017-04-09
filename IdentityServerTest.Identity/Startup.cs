e207ae267d125c04d11972ea745aecb4d217c1e6
Kimserey Lam <kimserey.lam@gmail.com>
2017-04-09 21:07:27 +0800

    first commit - added identity server

---
+++ IdentityServerTest.Identity/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace IdentityServerTest.Identity
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+			services.AddIdentityServer()
+			        .AddTemporarySigningCredential();
+		}
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+            app.UseDeveloperExceptionPage();
+
+			app.UseIdentityServer();
+        }
+    }
+}
