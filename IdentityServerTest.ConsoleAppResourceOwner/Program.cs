936de40015080ae2d2a2f368c451e1b0666cb6a2
Lam Kimserey <kimserey.lam@groomgy.com>
2017-04-12 23:17:02 +0800

    added example reference token - also confirmed that secret is needed with basic auth for authentication of client.

--- IdentityServerTest.ConsoleAppResourceOwner/Program.cs
+++ IdentityServerTest.ConsoleAppResourceOwner/Program.cs
@@ -5,13 +5,19 @@ using IdentityModel.Client;
 namespace IdentityServerTest.ConsoleAppResourceOwner
 {
 	class Program
-	{
-		public static async Task GetToken()
+    {
+        // Reference token example
+        // {
+        //    "access_token": "923da74fd0ce0c6ec67a75e5d5fbfae25911ac639acbcc270d34d6ecf652f641",
+        //    "expires_in": 3600,
+        //    "token_type": "Bearer"
+        // }
+        public static async Task GetToken()
 		{
 			var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
 			// request token
 			var tokenClient = new TokenClient(disco.TokenEndpoint, "website_1", "secret");
-			var tokenResponse = await tokenClient.RequestResourceOwnerPasswordAsync("alice", "password");
+            var tokenResponse = await tokenClient.RequestResourceOwnerPasswordAsync("alice", "password");
 
 			if (tokenResponse.IsError)
 			{
